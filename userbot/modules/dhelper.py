f291f045123d50ad12d313796cd6163d6dd3f7df
DIORrios285 <90284737+DIORrios285@users.noreply.github.com>
2021-09-22 07:10:14 +0800

    Update and rename rhelper.py to dhelper.py

--- userbot/modules/dhelper.py
+++ userbot/modules/dhelper.py
@@ -11,23 +11,23 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern="^.helpmy$")
 async def usit(e):
     await e.edit(
-        f"**Hai {DEFAULTUSER} Kalau Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.rhelp` Atau Bisa `.help` atau Minta Bantuan Ke:\n"
-        "\n[LANDAK🦔](t.me/teman_random)"
-        "\n\n[SUPPORT](https://t.me/geezsupportgroup)"
-        "\n\n[CHANNEL](https://t.me/ramubotinfo)")
+        f"**Hai {DEFAULTUSER} Kalau Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.helpme` Atau Bisa `.help` atau Minta Bantuan Ke:\n"
+        "\n[FATUR](t.me/uurfavboys1)"
+        "\n\n[SUPPORT](https://t.me/MBsokin)"
+        "\n\n[SPAM/HELP](https://t.me/diorspambot)")
 
 
 @register(outgoing=True, pattern="^.rvars$")
 async def var(m):
     await m.edit(
         f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://raw.githubusercontent.com/ramadhani892/RAM-UBOT/RAM-UBOT/varshelper.txt)")
+        "\n[DAFTAR VARS](https://raw.githubusercontent.com/DIORrios285/DIOR-UBOT/DIOR-UBOT/varshelper.txt)")
 
 
 CMD_HELP.update({
-    "ramhelper":
+    "diorhelper":
     "`.helpmy`\
-\nPenjelasan: Bantuan Untuk RAM-UBOT.\
+\nPenjelasan: Bantuan Untuk DIOR-UBOT.\
 \n`.rvars`\
 \nPenjelasan: Untuk Melihat Beberapa Daftar Vars."
 })
