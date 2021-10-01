f01ccb4f13ef3b1730dcbda321ff2f317ea3db27
DIORrios285 <90284737+DIORrios285@users.noreply.github.com>
2021-10-02 06:30:18 +0800

    Update kenalan.py

--- userbot/modules/kenalan.py
+++ userbot/modules/kenalan.py
@@ -12,7 +12,7 @@ async def typewriter(typew):
     sleep(3)
     await typew.edit("`Tinggal Di Kalimantan`")
     sleep(2)
-    await typew.edit("`**Gua ga jago, Tapi gua ganteng!**`")
+    await typew.edit("**Saya tidak jago, Tapi saya tampan!**")
 # Create by myself @localheart
 
 
