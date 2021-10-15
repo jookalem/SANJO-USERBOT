5c990fd1ca2f4499115419b2b7a62ee84f7e1a6a
Fatur <90284737+DIORrios285@users.noreply.github.com>
2021-10-15 12:37:11 +0800

    fixes bug

--- userbot/modules/diorupdate.py
+++ userbot/modules/diorupdate.py
@@ -55,7 +55,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
         if HEROKU_APP_NAME is None:
             await event.edit(
                 '`[HEROKU]: Harap Siapkan Variabel` **HEROKU_APP_NAME** `'
-                ' untuk dapat deploy perubahan terbaru dari RAM-USERBOT.`'
+                ' untuk dapat deploy perubahan terbaru dari DIOR-UBOT.`'
             )
             repo.__del__()
             return
