6ab7e9f5dbe46d54e1ea330a3703884601f08f83
Fatur <90284737+DIORrios285@users.noreply.github.com>
2021-11-24 03:18:34 +0800

    Update bannedall.py

--- userbot/modules/bannedall.py
+++ userbot/modules/bannedall.py
@@ -32,10 +32,30 @@ async def testing(event):
         await sleep(.5)
     await event.edit("Tidak Ada yang Terjadi di sini🙃🙂")
 
+
+@register(outgoing=True, pattern=r"^\.unbanall(?: |$)(.*)", groups_only=True)
+async def _(event):
+    await event.edit("`Sedang Mencari List Banning.`")
+    p = 0
+    (await event.get_chat()).title
+    async for i in event.client.iter_participants(
+        event.chat_id,
+        filter=ChannelParticipantsKicked,
+        aggressive=True,
+    ):
+        try:
+            await event.client.edit_permissions(event.chat_id, i, view_messages=True)
+            p += 1
+        except BaseException:
+            pass
+    await event.edit("`Sukses Menghapus List Banning`")
+
 CMD_HELP.update(
     {
         "bannedall": "**Plugin : **`cukup`\
     \n\n**Syntax : **`.bannedall`\
-    \n**Function : **ban all members in 1 cmnd"
+    \n**usage : **ban all members in 1 cmd\
+    \n\n**Syntax : **`.unbanall`\
+    \n**Usage : **unbanned semua member yang di ban dalam grup"
     }
 )
