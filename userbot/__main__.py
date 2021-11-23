51a0abc7036ff74d6533341063165d14bdd8c70d
Fatur <90284737+DIORrios285@users.noreply.github.com>
2021-11-23 12:20:01 +0800

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"⚡DIOR-UBOT⚡ 💦 V7.0 [AKTIF YE BANGSAT!!!!]")
+    f"⚡DIOR-UBOT⚡ 💦 V{BOT_VER} [AKTIF YE BANGSAT!!!!]")
 
 
 if len(argv) not in (1, 3, 4):
