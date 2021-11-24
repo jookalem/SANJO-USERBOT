6e41d1e6110c7bd74ef1e02b1876817d41a10a26
Fatur <90284737+DIORrios285@users.noreply.github.com>
2021-11-24 15:12:42 +0800

    Update and rename diordetek.py to deteksi.py

--- userbot/modules/deteksi.py
+++ userbot/modules/deteksi.py
@@ -1,6 +1,5 @@
 # Ported From Cat Userbot For Lord Userbot By Alvin/LiuAlvinas # Jangan Hapus # Jangan Ubah
 # Based On Plugins
-# bacot nyaaa
 
 
 from telethon.errors.rpcerrorlist import YouBlockedUserError
