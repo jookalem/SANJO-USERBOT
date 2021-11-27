e64674f2be14e1753dc014db77ab39f81baa6e30
Fatur <90284737+DIORrios285@users.noreply.github.com>
2021-11-27 11:14:09 +0800

    words

--- userbot/modules/sed.py
+++ userbot/modules/sed.py
@@ -101,7 +101,7 @@ async def sed(command):
 
 CMD_HELP.update({
     "sed":
-    ">`.s<delimiter><old word(s)><delimiter><new word(s)>`"
-    "\nUsage: Replaces a word or words using sed."
+    ">`.s<delimiter><kata lama(s)><delimiter><kata baru(s)>`"
+    "\nUsage: Mengganti kata atau kata menggunakan sed."
     "\nDelimiters: `/, :, |, _`"
 })
