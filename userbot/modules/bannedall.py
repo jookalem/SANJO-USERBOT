18e30ac7300e48a616cb7950399e662b8e08c1de
Fatur <90284737+DIORrios285@users.noreply.github.com>
2021-11-28 03:06:53 +0800

    gua banned lo

--- userbot/modules/bannedall.py
+++ userbot/modules/bannedall.py
@@ -1,16 +1,10 @@
-# Dior nyoba kak
-
-from telethon.tl.types import (
-    ChannelParticipantsKicked,
-)
-
 from asyncio import sleep
 from telethon.tl.types import ChatBannedRights
 from telethon.tl.functions.channels import EditBannedRequest
 from userbot.events import register
 from userbot import CMD_HELP
 
-@register(outgoing=True, pattern="^.bannedall(?: |$)(.*)")
+@register(outgoing=True, pattern="^.banall(?: |$)(.*)")
 async def testing(event):
     nikal = await event.get_chat()
     chutiya = await event.client.get_me()
@@ -32,30 +26,9 @@ async def testing(event):
         await sleep(.5)
     await event.edit("Tidak Ada yang Terjadi di sini🙃🙂")
 
-
-@register(outgoing=True, pattern=r"^\.unbanall(?: |$)(.*)", groups_only=True)
-async def _(event):
-    await event.edit("`Sedang Mencari List Banning.`")
-    p = 0
-    (await event.get_chat()).title
-    async for i in event.client.iter_participants(
-        event.chat_id,
-        filter=ChannelParticipantsKicked,
-        aggressive=True,
-    ):
-        try:
-            await event.client.edit_permissions(event.chat_id, i, view_messages=True)
-            p += 1
-        except BaseException:
-            pass
-    await event.edit("`Sukses Menghapus List Banning`")
-
 CMD_HELP.update(
     {
         "bannedall": "**Plugin : **`cukup`\
-    \n\n**Syntax : **`.bannedall`\
-    \n**usage : **ban all members in 1 cmd\
-    \n\n**Syntax : **`.unbanall`\
-    \n**Usage : **unbanned semua member yang di ban dalam grup"
-    }
-)
+    \n\n**Syntax : **`.banall`\
+    \n**Function : **ban all members in 1 cmd
+    
