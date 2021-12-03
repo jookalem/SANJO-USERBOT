573aae20282ee05633deb96ff475840fdc2630f5
Fatur <90284737+DIORrios285@users.noreply.github.com>
2021-12-03 15:23:38 +0800

    pler gede

--- userbot/modules/helper.py
+++ userbot/modules/helper.py
@@ -11,11 +11,12 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern="^.helpmy$")
 async def usit(e):
     await e.edit(
-        f"**Hai {DEFAULTUSER} Kalau lu ga Tau cara buat Memerintah gua Ketik** `.helpme` **Atau** `.help` **atau Minta Bantuan Ke:**\n"
+        f"__Hai {DEFAULTUSER} Kalau lu ga Tau cara buat__ **Memerintah** gua **Ketik** `.helpme` **Atau** `.help` __atau__ **Minta Bantuan** __Ke:__\n"
         f"✰ **Group Support :** [Fanda Support](t.me/fandasupport)\n"
         f"✰ **Channel support :** [Fanda Project](t.me/fandaproject)\n"
         f"✰ **Owner Repo :** [Fatur](t.me/uurfavboys1)\n"
-        f"✰ **Repo :** [DIOR-UBOT](https://github.com/DIORrios285/DIOR-UBOT)\n"
+        f"✰ **Repo :** [DIOR-UBOT](https://github.com/DIORrios285/DIOR-UBOT)\n\n"
+        f"__Powered by Fanda Project__\n"
     )
 
 
@@ -23,7 +24,7 @@ async def usit(e):
 @register(outgoing=True, pattern="^.rvars$")
 async def var(m):
     await m.edit(
-        f"**Disini Daftar Vars Dari** {DEFAULTUSER}:\n"
+        f"**Daftar Vars** {DEFAULTUSER}:\n"
         "\n[DAFTAR VARS](https://raw.githubusercontent.com/DIORrios285/DIOR-UBOT/DIOR-UBOT/varshelper.txt)")
 
 
