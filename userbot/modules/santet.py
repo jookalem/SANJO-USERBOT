feeda71ff8d93dc2d547d6831c94d4d7ce880177
Fatur <90284737+DIORrios285@users.noreply.github.com>
2021-12-03 15:55:55 +0800

    dukun

--- userbot/modules/santet.py
+++ userbot/modules/santet.py
@@ -6,12 +6,12 @@ from userbot.events import register
 @register(outgoing=True, pattern=r'^\.santet(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit(f"`{ALIVE_NAME} Pengen Nyantet Si Kontol Ini`")
+    await typew.edit(f"**{ALIVE_NAME} Pengen Nyantet Si Kontol Ini!**")
     sleep(2)
-    await typew.edit("`Mencari Nama Si Kontol...`")
-    sleep(1)
-    await typew.edit("`Santet Online Segera Dilakukan,Mampus Lu Jelek!!`")
-    sleep(1)
+    await typew.edit("__Mencari Nama Si Kontol...__")
+    sleep(2)
+    await typew.edit("__Santet Online Segera Dilakukan,Mampus Lu Jelek!!__")
+    sleep(2)
     await typew.edit("0%")
     number = 1
     await typew.edit(str(number) + "%   ▎")
@@ -313,9 +313,9 @@ async def typewriter(typew):
     sleep(0.03)
     await typew.edit(str(number) + "%   ████████████████▌")
     sleep(2)
-    await typew.edit("`Paku,Pisau,Gunting Jembut,Tanah Kuburan Bapak Lu,Tagihan Kost Udh Gua Masukin DiBadan Lu`")
-    sleep(4)
-    await typew.edit("`Si Jelek Udah Disantet Onlen:)`")
+    await typew.edit("__Paku, Pisau, Gunting Jembut, Tanah Kuburan Bapak Lu, Tagihan Kost Udh Gua Masukin Di Badan Lu__")
+    sleep(5)
+    await typew.edit("__Si Jelek Udah Disantet Onlen:)__")
 
 
 CMD_HELP.update({
