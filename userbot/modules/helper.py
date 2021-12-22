bf523a682dc5a1c3a5ee00cec433b61666669711
fatur tapi dior <90284737+DIORrios285@users.noreply.github.com>
2021-12-22 19:12:12 +0800

    Update helper.py

--- userbot/modules/helper.py
+++ userbot/modules/helper.py
@@ -11,9 +11,9 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern="^.helpmy$")
 async def usit(e):
     await e.edit(
-        f"__Hai {DEFAULTUSER} Kalau lu ga Tau cara buat__ **Memerintah** gua **Ketik** `.helpme` **Atau** `.help` __atau__ **Minta Bantuan** __Ke:__\n"
-        f"✰ **Group Support :** [Fanda Support](t.me/fandasupport)\n"
+        f"__Hai {DEFAULTUSER} Kalau lu ga Tau cara buat__ **Memerintah** gua **Ketik** `.helpme` Atau `.help` atau Minta **Bantuan** __Ke:__\n"
         f"✰ **Channel support :** [Fanda Project](t.me/fandaproject)\n"
+        f"✰ **Group Support :** [Fanda Support](t.me/fandasupport)\n"
         f"✰ **Owner Repo :** [Fatur](t.me/uurfavboys1)\n"
         f"✰ **Repo :** [DIOR-UBOT](https://github.com/DIORrios285/DIOR-UBOT)\n\n\n"
         f"__Powered by Fanda Project__"
