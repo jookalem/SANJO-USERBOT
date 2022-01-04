c7c2f69bfed2dd2c9d2d9fb2883d685065732bcd
SkyzuProject <92871568+Skyzu@users.noreply.github.com>
2022-01-04 17:44:10 +0700

    SKYZU GANTENG

--- userbot/modules/sangmata.py
+++ userbot/modules/sangmata.py
@@ -1,14 +1,9 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
-# Port to userbot by @MoveAngel
+from asyncio.exceptions import TimeoutError
 
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from userbot import bot, CMD_HELP
+
+from userbot import CMD_HELP, bot
 from userbot.events import register
-from asyncio.exceptions import TimeoutError
 
 
 @register(outgoing=True, pattern=r"^\.sg(?: |$)(.*)")
@@ -16,16 +11,16 @@ async def lastname(steal):
     if steal.fwd_from:
         return
     if not steal.reply_to_msg_id:
-        await steal.edit("```Mohon balas ke pesan pengguna```")
+        await steal.edit("```Mohon Balas Ke Pesan Pengguna.```")
         return
     message = await steal.get_reply_message()
     chat = "@SangMataInfo_bot"
     user_id = message.sender.id
     id = f"/search_id {user_id}"
     if message.sender.bot:
-        await steal.edit("```Mohon balas ke pesan pengguna```")
+        await steal.edit("```Balas Ke Pesan Pengguna Yang Sebenarnya.```")
         return
-    await steal.edit("LU SIAPA SI ANJING? GUA KEPO NGENTOT, GA SENENG PC!!.")
+    await steal.edit("**LU SIAPA SI ANJING? GUA KEPO NGENTOT, GA SENENG PC!!.**")
     try:
         async with bot.conversation(chat) as conv:
             try:
@@ -33,9 +28,7 @@ async def lastname(steal):
                 r = await conv.get_response()
                 response = await conv.get_response()
             except YouBlockedUserError:
-                await steal.reply(
-                    "```Mohon Unblock @sangmatainfo_bot Dan Coba Scan Kembali.```"
-                )
+                await steal.reply("```Mohon Unblock @sangmatainfo_bot Dan Coba Lagi```")
                 return
             if r.text.startswith("Name"):
                 respond = await conv.get_response()
@@ -47,7 +40,9 @@ async def lastname(steal):
             if response.text.startswith("No records") or r.text.startswith(
                 "No records"
             ):
-                await steal.edit("```bocah kaga ada riwayat namanya buseh dah, bocah tele baru lu ya jelek!```")
+                await steal.edit(
+                    "```Saya Tidak Menemukan Informasi Pengguna Ini, Pengguna Ini Belum Pernah Mengganti Nama Sebelumnya```"
+                )
                 await steal.client.delete_messages(
                     conv.chat_id, [msg.id, r.id, response.id]
                 )
@@ -59,11 +54,12 @@ async def lastname(steal):
                 conv.chat_id, [msg.id, r.id, response.id, respond.id]
             )
     except TimeoutError:
-        return await steal.edit("`Saya Sedang Sakit, Mohon Maaf`")
+        return await steal.edit("`Saya Sedang Sakit Mohon Maaf`")
 
 
-CMD_HELP.update({
-    "sangmata":
-        "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.sg`\
-          \n↳ : Mendapatkan Riwayat Nama Pengguna Yang Di Scan."
-})
+CMD_HELP.update(
+    {
+        "sangmata": "`.sg`\
+          \nUsage: Mendapatkan Riwayat Nama Pengguna."
+    }
+)
