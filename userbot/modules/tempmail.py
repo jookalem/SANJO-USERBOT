4b1735f98c44c66f96edf737972b7d0bdf70f387
fatur tapi dior <90284737+DIORrios285@users.noreply.github.com>
2022-01-16 08:41:28 +0800

    Update tempmail.py

--- userbot/modules/tempmail.py
+++ userbot/modules/tempmail.py
@@ -1,41 +1,37 @@
-# BASED FROM ULTROID PORTED FOR LORD USERBOT BY ALVIN / @LIUALVINAS
-# THANKS ULTROID
-# DONT REMOVE THIS
-# ALVIN GANTENG
-# @LORDUSERBOT_GROUP
-
-from telethon import events
-from userbot import CMD_HELP, bot
-from userbot.events import register
-from telethon.errors.rpcerrorlist import YouBlockedUserError
 import asyncio
 
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
 
-@register(outgoing=True, pattern=r"^\.te(?: |$)(.*)")
+from userbot import CMD_HELP, bot
+from userbot.events import register
+
+
+@register(outgoing=True, pattern=r"^\.tm(?: |$)(.*)")
 async def _(event):
     chat = "@TempMailBot"
-    lord = await event.edit("Sedang Memprosess...")
+    geez = await event.edit("Sedang Memprosess...")
     async with bot.conversation(chat) as conv:
         try:
-            response = conv.wait_event(events.NewMessage(
-                incoming=True,
-                from_users=1937084611
-            )
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=220112646)
             )
             await conv.send_message("/start")
             await asyncio.sleep(1)
             await conv.send_message("Generate New")
             response = await response
-            lorduserbot = ((response).reply_markup.rows[2].buttons[0].url)
+            geezuserbot = (response).reply_markup.rows[2].buttons[0].url
             await event.client.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await lord.edit("`Mohon buka blokir` @TempMailBot `lalu coba lagi`")
+            await geez.edit(
+                "`Mohon Maaf, Silahkan Buka` @TempMailBot `Lalu Tekan Start dan Coba Lagi.`"
+            )
             return
-        await event.edit(f"**DIOR TEMPMAIL** ~ `{response.message.message}`\n\n[KLIK DISINI UNTUK VERIFIKASI]({lorduserbot})")
+        await event.edit(
+            f"**GEEZ TEMPMAIL** ~ `{response.message.message}`\n\n[KLIK DISINI UNTUK VERIFIKASI]({geezuserbot})"
+        )
 
 
-# Alvin Ganteng
-# Ported For Lord Userbot From Ultroid
-
-CMD_HELP.update({"tempmail": "**Modules:** __Temp Mail__\n\n**Perintah:** `.te`"
-                 "\n**Penjelasan:** Mendapatkan Email Gratis Dari Temp Mail"})
+CMD_HELP.update(
+    {"tempmail": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.tm`" "\n•: Mendapatkan Email Gratis Dari Temp Mail"}
+)
