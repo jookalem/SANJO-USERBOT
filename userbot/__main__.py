12c9ad424368ece4c6d818b1c8c0adb7362a5af6
IKHSAN GANTENG <97096980+jookalem@users.noreply.github.com>
2022-01-16 18:30:33 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"⚡DIOR-UBOT⚡ ⚙️ V{BOT_VER} [BERHASIL DI AKTIFKAN PLER!]")
+    f"⚡SANJO-USERBOT⚡ ⚙️ V{BOT_VER} [BERHASIL DI AKTIFKAN PLER!]")
 
 
 if len(argv) not in (1, 3, 4):
